4cbd9f927cbe1b436bebea029becb0439c423073
arunkarthik0712 <arunkarthik0710@gmail.com>
2024-07-31 16:45:03 +0530

    main jsx files

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./Register";
+import ForgotPassword from "./ForgotPassword";
+import ResetPassword from "./ResetPassword";
+import Login from "./Login";
+
+const App = () => {
+  return (
+    <Router>
+      <div className="container">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
